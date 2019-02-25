d0424a68af3fdd6abdd3de3bcde3fc14062f97bc
Jadelette <jade.a.corcoran@gmail.com>
2019-02-25 15:30:57 +0000

    internal server error - frustrating

--- dbservice.js
+++ dbservice.js
@@ -1,1 +1,37 @@
 const mysql = require("mysql");
+
+function getDatabaseConnection() {
+    return mysql.createConnection({
+        host: process.env.RDS_HOST,
+        user: process.env.RDS_USER,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE //environment variables - used for security and flexibility
+    });
+}
+
+function sendQuery(query, params) {
+    const connection = getDatabaseConnection();
+    return new Promise(function(resolve, reject) {
+        connection.query(query, params, function(error, results, fields) {
+            if (error) {
+                connection.destroy();
+                return reject(error);
+            } 
+            else {
+                connection.end();
+                return resolve(results);
+            }
+        });
+    });
+}
+
+function getResources(){
+    const query = "SELECT * FROM learning_resources";
+    return sendQuery(query);
+}
+
+
+
+module.exports = {
+    getResources
+}
--- resources.js
+++ resources.js
@@ -1,5 +1,20 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+app.use(express.json());
 const dbService = require('./dbservice');
+
+app.get('/resources', function (request, response) {
+    dbService.getResources()
+    .then(function(results){
+      //we got the resources ok
+      response.json(results);
+    })
+    .catch(function(error){
+      //something went wrong
+      response.status(500);
+      response.json(error);
+    });
+  })
+
+module.exports.handler = serverless(app);
