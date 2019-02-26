abb0f76c55fda30c06f658a293a617548dd55abf
Nicola Buckhurst <nicolabuckhurst@gmail.com>
2019-02-26 13:49:27 +0000

    added some sql files

---
+++ sql/saving_a_resource.sql
@@ -0,0 +1,22 @@
+#ADD A NEW RESOURCE TO DATABASE - 3 STEP QUERY 
+
+#STEP ONE: SAVE ALL THE RESOURCE DETAILS INTO resources TABLE -- this is just like saving the todoitem but with
+#a few extra fields
+# when we run this query from the Node mySQL package you get the autoIncremented Id of the insert returned to you as a response
+# from database.....so you can capture that in a variable ready to use in step2!
+# https://github.com/mysqljs/mysql#getting-the-id-of-an-inserted-row
+USE learning_dashboard;
+INSERT INTO resources (title, description, url, userName) 
+VALUES ("JavaScript Tutorial", "This is a basic JavaScript Tutorial", "test@test.com", "Nicola");
+
+
+#STEP TWO: LOOK UP THE TAG IDs for the Tagnames associated with the resource 
+#when we run this query from backend code we will get the list of tag_ids we want to give to the resource
+USE learning_dashboard;
+SELECT tagId from tags WHERE tagName IN ('Javascript', 'Loops');
+
+#STEP THREE: PUT ENTRIES INTO taggings TABLE FOR EACH resourceId(from step 1) and tagId(from Step 2)
+USE learning_dashboard;
+INSERT INTO taggings (resourceId, tagId) 
+VALUES (2, 1),(2,12);
+
